f39a26f8cf22bd7a8638bbabf688f3c3ad8a24b7
Wildann77 <boyblanco77@gmail.com>
2026-02-18 02:09:19 +0700

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
     return (
       <div className="min-h-screen flex items-center justify-center">
         <Loader2 className="h-8 w-8 animate-spin text-primary" />
-      </div>
+    </div>
     );
   }
 
